1d165a8ff56e8cf831cb36c9f8d6099223fcc8fd
Pozole10 <caritogold17@gmail.com>
2025-04-09 07:15:44 -0600

    Frontend jajiji

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 10000,
+    host: '0.0.0.0',
+    allowedHosts: ['front-kppe.onrender.com'] // 👈 Agrega esto
+  }
 })
